3bc42bbb08d44a9b3bbf47a7c8247b477884bb0f
Daniel Morgan <daniel.morgan@nielsen.com>
2020-06-14 15:12:30 +0100

    this now builds but needs some --init --cap-add=SYS_ADMIN so not pushing to docker hub yet

--- strava.js
+++ strava.js
@@ -34,11 +34,12 @@ const puppeteer = require('puppeteer')
 const debug = false;
 
 (async () => {
-  const headless = !(debug === false)
+  const headless = !(debug === true)
   const browser = await puppeteer.launch({
     headless: headless,
     args: [
       // Required for Docker version of Puppeteer
+      //'--no-sandbox',
       '--disable-setuid-sandbox',
       // This will write shared memory files into /tmp instead of /dev/shm,
       // because Docker’s default for /dev/shm is 64MB
