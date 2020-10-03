494cc1143660d23273c6a297c95cc5ad405951fb
Daniel Morgan <daniel@morgan.cymru>
2020-10-03 20:19:07 +0100

    checkmark before adding caching (#7)

--- strava.js
+++ strava.js
@@ -7,6 +7,37 @@
  *
  */
 
+const secrets = require("./secrets.json")
+
+const username = (process.env.STRAVA_USER || secrets.STRAVA_USER)
+const password = (process.env.STRAVA_PWD || secrets.STRAVA_PWD)
+
+const {
+  performance,
+  PerformanceObserver
+} = require('perf_hooks');
+
+async function spinTimer(timeToWait){
+  let CLI = require('clui'),
+      Spinner = CLI.Spinner;
+
+  let countdown = new Spinner(`Waiting ${timeToWait} seconds...  `, ['⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷']);
+
+  countdown.start();
+
+  let number = timeToWait;
+  for(;;){
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    number--;
+    countdown.message('Waiting ' + number + ' seconds...  ');
+    if (number === 0) {
+      process.stdout.write('\n');
+      countdown.stop();
+      return
+    }
+  } 
+}
+
 class InflightRequests {
   constructor(page) {
     this._page = page;
@@ -33,6 +64,7 @@ class InflightRequests {
 require('console-stamp')(console, '[HH:MM:ss.l]');
 
 const puppeteer = require('puppeteer')
+const request_client = require('request-promise-native');
 const chalk = require('chalk')
 
 const debug = false;
@@ -40,18 +72,23 @@ const debug = false;
 (async () => {
   const headless = !(debug === true)
   const browser = await puppeteer.launch({
+    userDataDir: '/tmp/user-data-dir',
     headless: headless,
     args: [
       // Required for Docker version of Puppeteer
-      //'--no-sandbox',
-      '--disable-setuid-sandbox',
+      '--no-sandbox',
+      //'--disable-setuid-sandbox',
       // This will write shared memory files into /tmp instead of /dev/shm,
       // because Docker’s default for /dev/shm is 64MB
-      '--disable-dev-shm-usage'
+      //'--disable-dev-shm-usage'
     ]
   })
   const page = await browser.newPage()
   const tracker = new InflightRequests(page);
+
+  if(debug===true){
+    await page.setRequestInterception(true);
+  }
 
 
   page
@@ -74,69 +111,123 @@ const debug = false;
           }
           const color = colors[colorType] || chalk.blue
           console.log(color(`type: ${type}, subtype: ${subtype}, description:\n ${description}`))
-        }const chalk = require('chalk')
+        }
       })
     })
     .on('pageerror', ({ message }) => console.log(chalk.red(message)))
     .on('response', response =>
       console.log(chalk.green(`${response.status()} ${response.url()}`)))
     .on('requestfailed', request =>
-      console.log(chalk.magenta(`${request.failure().errorText} ${request.url()}`)))
+      console.log(chalk.magenta(`${request.failure().errorText} ${request.url()}`)));
+    if(debug==true){
+      page.on('request', request => {
+          request_client({
+            uri: request.url(),
+            resolveWithFullResponse: true,
+            }).then(response => {
+              // if(debug===true && request.url() == "https://www.strava.com/session") {
+              //   console.log("REQUEST_URL")
+              //   console.log(chalk.magenta(request.url()));
+              //   const request_headers = request.headers();
+              //   console.log("REQUEST_HEADERS");
+              //   console.log(request.headers());
+              //   console.log("POST_DATA");
+              //   console.log(request.postData());
+              //   console.log("RESPONSE_HEADERS")
+              //   const response_headers = response.headers;
+              //   console.log(response_headers);
+              //   console.log("RESPONSE");
+              //   console.log(response)
+              //   console.log("RESPONSE_LENGTH");
+              //   console.log(chalk.magenta(response_headers['content-length']));
+              // }300
+              request.continue();
+            }).catch(error => {
+              console.error(error);
+              request.abort();
+            });
+        });
+    }
 
   try {
-    await page.goto('https://www.strava.com/login').catch(e => {
-      console.log('Navigation failed: ' + e.message);
-      const inflight = tracker.inflightRequests();
-      console.log(inflight.map(request => '  ' + request.url()).join('\n'))
-    });
-    tracker.dispose();
-    console.log('Got to strava')
-
-    await page.click('#email')
-    await page.keyboard.type(process.env.STRAVA_USER)
-    console.log('Entered Username')
-
-
-    await page.click('#password')
-    await page.keyboard.type(process.env.STRAVA_PWD)
-    console.log('Entered Password')
-
-    await page.$eval('#login_form', form => form.submit())
-    console.log('Submitted Form')
-    await new Promise(resolve => setTimeout(resolve, 20000))
-    console.log('Waited Now Navigating')
-
-    await page.goto('https://www.strava.com/dashboard/following/300', {waitUntil: 'networkidle2',
-      timeout: 3000000}).catch(e => {
-        console.log('Navigation failed: ' + e.message);
-        const inflight = tracker.inflightRequests();
-        console.log(inflight.map(request => '  ' + request.url()).join('\n'));
+    let howManyLooped = 0
+    let howManySecondsPuppeting = 0
+    for(;;) {
+      var t0 = performance.now()
+      await page.goto('https://www.strava.com/dashboard/following/300', {
+        waitUntil: 'networkidle2',
+        timeout: 3000000}).catch(e => {
+          console.log('Navigation failed: ' + e.message);
+          const inflight = tracker.inflightRequests();
+          console.log(inflight.map(request => '  ' + request.url()).join('\n'))
       });
       tracker.dispose();
+      console.log('Got to strava')
 
-    await new Promise(resolve => setTimeout(resolve, 20000))
+      try {
+        await page.waitForSelector('#login-button', {
+          timeout: 2000
+        })
 
+        await page.click('#email')
+        await page.keyboard.type(username)
+        console.log('Entered Username')
 
-    console.log('Lets Click Some Buttons')
-    await page.evaluate(() => {
-      let howManyClicked = 0
-      const kudosButtons = Array.from(document.querySelectorAll('button.js-add-kudo'))
-      for (let i = 0; i < kudosButtons.length; i++) {
-        try {
-          console.log(`clicking ${i}`)
-          howManyClicked++
-          kudosButtons[i].click()
-        } catch (error) {
-          console.log(error)
-        }
+        await page.click('#password')
+        await page.keyboard.type(password)
+        console.log('Entered Password')
+        
+    
+        await page.click('#login-button')
+        console.log('Submitted Form')
+    
+        await page.waitForNavigation({
+          waitUntil: 'networkidle2',
+          timeout: 3000000
+        });
+        console.log('Waited Now Kudo')
+
+      } catch (error) {
+        console.log("Looks like we were already logged in :D.")
       }
-      console.log(howManyClicked)
-    })
-    if (debug === true) {
-      console.log('taking a screenshot')
-      await new Promise(resolve => setTimeout(resolve, 5000))
-      await page.screenshot({ path: 'strava.png', fullPage: true })
-      await new Promise(resolve => setTimeout(resolve, 5000))
+
+
+      // const cookies = await page.cookies()
+      // console.log(b4cookies)
+      // process.exit(0);
+
+
+      console.log('Lets Click Some Buttons')
+      await page.evaluate(() => {
+        let howManyClicked = 0
+        const kudosButtons = Array.from(document.querySelectorAll('button.js-add-kudo'))
+        for (let i = 0; i < kudosButtons.length; i++) {
+          try {
+            console.log(`clicking ${i}`)
+            howManyClicked++
+            kudosButtons[i].click()
+          } catch (error) {
+            console.log(error)
+          }
+        }
+        console.log(howManyClicked)
+      })
+      // if (debug === true) {page.waitForSelectora.png', fullPage: true })
+      //   await new Promise(resolve => setTimeout(resolve, 5000))
+      // }
+      const ceiling = 1800
+      const weWillWait = Math.floor(Math.random() * ceiling) + 1 ;
+      var t1 = performance.now()
+
+      if (howManySecondsPuppeting != 0) {
+        let averagePupeteeringTime = howManySecondsPuppeting/howManyLooped;
+        console.log(`Average Pupeteering Time ${averagePupeteeringTime} milliseconds`);
+      }
+      console.log(`we will wait ${weWillWait} seconds`);
+      await spinTimer(weWillWait);
+      howManySecondsPuppeting += (t1 - t0)
+      howManyLooped++;
+      console.log(`we've looped ${howManyLooped}`);
     }
   } catch (error) {
     console.log(error)
